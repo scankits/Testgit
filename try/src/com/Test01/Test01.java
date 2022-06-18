6faa93db6844a97c27c0e321c0aaea9a01019366
scankit <228465734@qq.com>
2022-06-18 22:28:12 +0800

    第一次使用IDEA提交库

---
+++ try/src/com/Test01/Test01.java
@@ -0,0 +1,28 @@
+package com.Test01;
+
+import java.util.Scanner;
+
+public class Test01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("请输入被除数：");
+        if (input.hasNextInt()){
+            int num1 = input.nextInt();
+            System.out.print("请输入除数：");
+            if(input.hasNextInt()){
+                int num2 = input.nextInt();
+                if (num2 != 0){
+                    System.out.println("商："+num1/num2);
+                }else{
+
+                }
+            }
+        }else{
+            System.out.println("请输入一个整数！");
+        }
+
+
+    }
+
+}
